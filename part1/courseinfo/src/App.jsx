dedaf029c26ca35152ffc1d721d0d666a2c67727
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-20 21:22:24 +0100

    feat: courseinfo - part 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,9 +5,9 @@ const Header = (props) => {
 const Content = (props) => {
 	return (
 		<>
-			<Part part={props.part1} />
-			<Part part={props.part2} />
-			<Part part={props.part3} />
+			{props.parts.map((part) => (
+				<Part part={part} key={part.name} />
+			))}
 		</>
 	);
 };
@@ -27,26 +27,29 @@ const Total = (props) => {
 const App = () => {
 	const course = "Half Stack application development";
 
-	const part1 = {
-		name: "Fundamentals of React",
-		exercises: 10,
-	};
+	const parts = [
+		{
+			name: "Fundamentals of React",
+			exercises: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercises: 7,
+		},
+		{
+			name: "State of a component",
+			exercises: 14,
+		},
+	];
 
-	const part2 = {
-		name: "Using props to pass data",
-		exercises: 7,
-	};
+	const total = parts.reduce((accu, current) => accu + current.exercises, 0);
 
-	const part3 = {
-		name: "State of a component",
-		exercises: 14,
-	};
-
+	console.log(total);
 	return (
 		<div>
 			<Header course={course} />
-			<Content part1={part1} part2={part2} part3={part3} />
-			<Total total={part1.exercises + part2.exercises + part3.exercises} />
+			<Content parts={parts} />
+			<Total total={total} />
 		</div>
 	);
 };
