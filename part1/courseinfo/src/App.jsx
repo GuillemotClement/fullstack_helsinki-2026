46712ff6c724f54a8936957d614538c3c05d3137
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-20 21:31:30 +0100

    refacto: courseinfo - part5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-	return <h1>{props.course}</h1>;
+	return <h1>{props.name}</h1>;
 };
 
 const Content = (props) => {
@@ -25,30 +25,33 @@ const Total = (props) => {
 };
 
 const App = () => {
-	const course = "Half Stack application development";
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
 
-	const parts = [
-		{
-			name: "Fundamentals of React",
-			exercises: 10,
-		},
-		{
-			name: "Using props to pass data",
-			exercises: 7,
-		},
-		{
-			name: "State of a component",
-			exercises: 14,
-		},
-	];
+	const total = course.parts.reduce(
+		(accu, current) => accu + current.exercises,
+		0,
+	);
 
-	const total = parts.reduce((accu, current) => accu + current.exercises, 0);
-
-	console.log(total);
 	return (
 		<div>
-			<Header course={course} />
-			<Content parts={parts} />
+			<Header name={course.name} />
+			<Content parts={course.parts} />
 			<Total total={total} />
 		</div>
 	);
