d93e651cb3f1005c2e50020e24afbf6383816ff7
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-21 14:31:34 +0100

    feat: courseinfo - part 7 et 8

--- part2/courseinfo/src/components/course/Content.jsx
+++ part2/courseinfo/src/components/course/Content.jsx
@@ -1,14 +1,21 @@
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ course }) => {
 	const { parts } = course;
+	const total = parts.reduce((acc, value) => acc + value.exercises, 0);
 
 	return (
-		<div>
-			{parts.map((part) => (
-				<Part key={part.id} part={part} />
-			))}
-		</div>
+		<>
+			<div>
+				{parts.map((part) => (
+					<Part key={part.id} part={part} />
+				))}
+			</div>
+			<div className="">
+				<Total total={total} />
+			</div>
+		</>
 	);
 };
 
---
+++ part2/courseinfo/src/components/course/Total.jsx
@@ -0,0 +1,5 @@
+const Total = ({ total }) => {
+	return <b>Total of {total} exercices</b>;
+};
+
+export default Total;
