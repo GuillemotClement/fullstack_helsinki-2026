f0e5988ec93268d119a3d2cc63b9ab8546d04a06
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-21 09:28:34 +0100

    feat: unicafe - part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const Title = ({ text }) => {
+	return <h2>{text}</h2>;
+};
+
+const Button = ({ text, onClick }) => {
+	return (
+		<button type="button" onClick={onClick}>
+			{text}
+		</button>
+	);
+};
+
+const Result = ({ text, result }) => {
+	return (
+		<p>
+			{text} {result}
+		</p>
+	);
+};
+
+const App = () => {
+	// enregistrer les clics de chaque bouton dans un état différent
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const handleClickGood = () => setGood(good + 1);
+	const handleClickNeutral = () => setNeutral(neutral + 1);
+	const handleClickBad = () => setBad(bad + 1);
+
+	return (
+		<div>
+			<Title text="give feedback" />
+			<div className="">
+				<Button text="good" onClick={handleClickGood} />
+				<Button text="neutral" onClick={handleClickNeutral} />
+				<Button text="bad" onClick={handleClickBad} />
+			</div>
+			<Title text="statistics" />
+			<div className="">
+				<Result text="good" result={good} />
+				<Result text="neutral" result={neutral} />
+				<Result text="bad" result={bad} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
