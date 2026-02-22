9f216c83a0729815a033e4a12af34e18a30cf5ee
GuillemotClement <guillemot.clement@protonmail.com>
2026-02-22 20:27:32 +0100

    feat: phonebook - part3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,55 +1,76 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", tel: "0645231586" },
+	]);
+	const [newName, setNewName] = useState("");
+	const [newTel, setNewTel] = useState("");
 
-  const addNumber = (event) => {
-    event.preventDefault();
+	const addNumber = (event) => {
+		event.preventDefault();
 
-    let isExist = false;
+		let isExist = false;
 
-    persons.forEach((person) => {
-      if (person.name === newName) {
-        alert(`${newName} is already added to phonebook`);
-        isExist = true;
-        setNewName("");
-      }
-    });
+		persons.forEach((person) => {
+			if (person.name === newName) {
+				alert(`${newName} is already added to phonebook`);
+				isExist = true;
+				setNewName("");
+				setNewTel("");
+			}
+		});
 
-    if (isExist) {
-      return;
-    }
+		if (isExist) {
+			return;
+		}
 
-    const personName = {
-      name: newName,
-    };
+		const personName = {
+			name: newName,
+			tel: newTel,
+		};
 
-    setPersons(persons.concat(personName));
-    setNewName("");
-  };
+		setPersons(persons.concat(personName));
+		setNewName("");
+		setNewTel("");
+	};
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
-        <div>
-          name: <input onChange={handleNameChange} value={newName} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
-    </div>
-  );
+	const handleTelChange = (event) => {
+		setNewTel(event.target.value);
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addNumber}>
+				<div>
+					name: <input onChange={handleNameChange} value={newName} required />
+				</div>
+				<div className="">
+					<label htmlFor="">Number : </label>
+					<input
+						type="tel"
+						onChange={handleTelChange}
+						value={newTel}
+						required
+					/>
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<p key={person.name}>
+					{person.name} - {person.tel}
+				</p>
+			))}
+		</div>
+	);
 };
 
 export default App;
